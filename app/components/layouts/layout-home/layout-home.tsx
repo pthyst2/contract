d63894957be5f6e05b3fe2eadb6550e22be380ca
Quân (Máy cty) <pthyst2@gmail.com>
2022-09-13 16:46:42 +0700

    I feel good

--- app/components/layouts/layout-home/layout-home.tsx
+++ app/components/layouts/layout-home/layout-home.tsx
@@ -1,11 +1,3 @@
-import { Header } from "../../partials/header/header";
-import { Footer } from "../../partials/footer/footer";
 export function LayoutHome({ children }: any) {
-  return (
-    <div className="flex flex-col min-h-[100vh]">
-      <Header />
-      <main className="grow">{children}</main>
-      <Footer />
-    </div>
-  );
+  return <></>;
 }
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import SeoHead from "../components/partials/seo-head/seo-head";
-import { LayoutHome } from "../components/layouts/layout-home/layout-home";
-import { SectionQuickAction } from "../components/partials/section-quick-action/section-quick-action";
 
 const Home: NextPage = () => {
   //#region SEO
@@ -12,16 +11,90 @@ const Home: NextPage = () => {
   return (
     <>
       <SeoHead input={seo} />
-      <LayoutHome>
-        <div className="bg-white">
-          {/* Quick actions */}
-          <SectionQuickAction />
-          {/* end of quick actions */}
-          {/* Main content */}
-          <div className="container mx-auto py-[2em]"></div>
-          {/* end of main content */}
+      <main className="h-[100vh] w-full flex items-center">
+        {/* Sign Contract */}
+        <div className="big-bg sign-contract">
+          <div className="text-white max-w-[350px] xl:max-w-[35%]">
+            <p className="font-delaGothicOne text-[18px] md:text-[24px] xl:text-[36px]">
+              I have contracts need to sign !
+            </p>
+            <div className="buttons py-4">
+              <Link href="">
+                <a className="rounded-[4px] text-[13px] btn btn-white py-2 px-4 mb-2">
+                  Sign In
+                </a>
+              </Link>
+              <Link href="">
+                <a className="rounded-[4px] text-[13px] btn btn-second py-2 px-4">
+                  Go Contracts
+                </a>
+              </Link>
+            </div>
+          </div>
         </div>
-      </LayoutHome>
+        {/* end of sign contract */}
+        {/* Create Contract */}
+        <div className="big-bg create-contract">
+          <div className="text-white max-w-[350px] xl:max-w-[35%]">
+            <p className="font-delaGothicOne text-[18px] md:text-[24px] xl:text-[36px]">
+              新しい契約を作成したい！
+            </p>
+            <div className="buttons py-4">
+              <Link href="">
+                <a className="rounded-[4px] text-[13px] btn btn-white py-2 px-4 mb-2">
+                  ログイン
+                </a>
+              </Link>
+              <Link href="">
+                <a className="rounded-[4px] text-[13px] btn btn-main py-2 px-4">
+                  新規契約の作成
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+        {/* end of create contract */}
+      </main>
+
+      {/* Style */}
+      <style jsx>{`
+        .big-bg {
+          display: inline-flex;
+          width: 50%;
+          height: 100%;
+          flex-shrink: 1;
+          justify-content: center;
+          align-items: center;
+          background-size: cover;
+          transition: all 0.75s ease-in-out;
+        }
+        .big-bg:hover {
+          width: 100%;
+        }
+        .big-bg.sign-contract {
+          background-image: url("/images/solid-colors/second.jpg");
+        }
+        .big-bg.sign-contract:hover {
+          background-image: url("/images/sign-contract.jpg");
+        }
+        .big-bg.create-contract {
+          background-image: url("/images/solid-colors/main.jpg");
+        }
+        .big-bg.create-contract:hover {
+          background-image: url("/images/create-contract.jpg");
+        }
+        .big-bg .buttons {
+          display: none;
+        }
+        .big-bg:hover .buttons {
+          display: block;
+        }
+        .big-bg .buttons .btn {
+          display: flex;
+          width: 100%;
+        }
+      `}</style>
+      {/* end of style */}
     </>
   );
 };
